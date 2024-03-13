58abd86a024da66cc274eb80f72d4bd630436e8c
debjitseth <dseth031@uottawa.ca>
2024-03-13 16:44:34 -0400

    connect to db and oull information from it

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/searchbar"
 import Image from "next/image"
-const Home = () => {
-  return (
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-    //wrapping everything in a react style container
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
     <>
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
-              Smart Shoping Starts Here:
+              Smart Shopping Starts Here:
               <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
@@ -18,27 +21,29 @@ const Home = () => {
                 height={16}
               />
             </p>
-            <h1 className = "head-text">
-              Unleash the power of
-              <span className = "text-primary"> Pricewise</span>
+
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> PriceWise</span>
             </h1>
-            <p className = "mt-6">
-            Powerful, self-serve 
-            product and growth analytics to help you 
-            convert, engage, and retain more.
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
+
             <Searchbar />
           </div>
-          <HeroCarousel />
 
+          <HeroCarousel />
         </div>
       </section>
-      <section className = "trending-section">
-        <h2 className = "section-text">Trending</h2>
-        <div className = "flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple iPhone 15','Book','Sneakers'].map
-          ((product) =>(
-            <div>{product}</div>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product: any) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,23 +2,75 @@
 
 import { scapeAmazonProduct } from "../scraper";
 import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { revalidatePath } from "next/cache";
 
-export async function scrapeAndStoreProduct(productUrl: string){
-    if(!productUrl) return;
+export async function scrapeAndStoreProduct(productUrl: string) {
+    if (!productUrl) return;
 
     //try catch block as we are dealing with async and database actions
     try {
         connectToDB();
         const scrapedProduct = await scapeAmazonProduct(productUrl);
 
-        if(!scrapedProduct) return;
+        if (!scrapedProduct) return;
 
         let product = scrapedProduct;
 
-        //const existngProduct = await Product
+        const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-    } catch (error:any) {
+        if(existingProduct){
+            const updatedPriceHistory: any = [
+                ...existingProduct.priceHistory,
+                { price: scrapedProduct.currentPrice}
+            ]
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory),
+
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            {url: scrapedProduct.url},
+            product,
+            {upsert: true, new: true}
+        );
+
+
+            revalidatePath(`/products/${newProduct._id}`);
+    } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
+
+    }
+}
+
+export async function getProductById(productId: string){
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({_id: productId});
+        if(!product) return null;
+        
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getAllProducts(){
+    try {
+        connectToDB();
+
+        const products = await Product.find();
+
+        return products;
+    } catch (error) {
+        console.log(error);
         
     }
 }
