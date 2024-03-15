51c51e58322f5c0805df73907535da989b59ae32
debjitseth <dseth031@uottawa.ca>
2024-03-14 23:52:51 -0400

    inplemented email changes, page changes and final website layout

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import { connectToDB } from "../mongoose";
 import Product from "../models/product.model";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { revalidatePath } from "next/cache";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if (!productUrl) return;
@@ -74,3 +76,44 @@ export async function getAllProducts(){
         
     }
 }
+
+export async function getSimilarProducts(productId: string) {
+    try {
+      connectToDB();
+  
+      const currentProduct = await Product.findById(productId);
+  
+      if(!currentProduct) return null;
+  
+      const similarProducts = await Product.find({
+        _id: { $ne: productId },
+      }).limit(3);
+  
+      return similarProducts;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+      const product = await Product.findById(productId);
+  
+      if(!product) return;
+  
+      const userExists = product.users.some((user: User) => user.email === userEmail);
+  
+      if(!userExists) {
+        product.users.push({ email: userEmail });
+  
+        await product.save();
+  
+        const emailContent = await generateEmailBody(product, "WELCOME");
+  
+        await sendEmail(emailContent, [userEmail]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
